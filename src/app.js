b579a2e43518cd8310d793c5712a9638c3c1377f
unknown <vignesh.pingili@gmail.com>
2022-04-25 12:04:52 +0530

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
 const app = express()
+const port = process.env.PORT || 3000
+
 //define path for express config
 const publicDirpath = path.join(__dirname,'../public')
 const viewpath = path.join(__dirname,'../vignesh/views/')
@@ -97,6 +99,6 @@ app.get('*',(req,res)=>{
 // })
 
 app.get('')
-app.listen(3000,()=>{
+app.listen(port,()=>{
   console.log('server is up on port 3000')
 })
